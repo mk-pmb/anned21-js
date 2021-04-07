8ade743449b14277088b14b9fb2384a2fb82c273
mk-pmb <mk-pmb@users.noreply.github.com>
2021-04-07 23:51:10 +0200

    Trying systemjs for module loading

---
+++ src/standalone.load_inplace.mjs
@@ -0,0 +1,10 @@
+﻿// -*- coding: utf-8, tab-width: 2 -*-
+
+import cash from 'cash-dom';
+import JSON5 from 'json5';
+
+Array.from(cash('.anned21-standalone')).forEach(function initOne(dest) {
+  const cfg = (JSON5.parse(cash(dest).find('script.config').html()) || {});
+  dest.config = cfg; // eslint-disable-line no-param-reassign
+  cash(dest).find('script').remove();
+});
